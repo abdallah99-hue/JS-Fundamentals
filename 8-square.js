4abf46613a28cfd49def9d08e89abf48081a6401
abdallah99-hue <abdallahbenomar99@gmail.com>
2025-11-13 16:25:36 -0800

    add file 8-square.js

---
+++ 8-square.js
@@ -0,0 +1,1 @@
+const args = process.argv.slice(2); if (!args[0] || isNaN(parseInt(args[0], 10))) { console.log("Missing size"); } else { const size = parseInt(args[0], 10); for (let i = 0; i < size; i++) { console.log("X".repeat(size)); } }
